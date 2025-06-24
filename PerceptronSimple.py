1b413c2c58449359ee3119a063d50655670d34ba
new54 <newgear54@gmail.com>
2025-06-24 09:23:05 -0500

    Creacion del PerceptronSimple

---
+++ PerceptronSimple.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pandas as pd
+
+class PerceptronSimple:
+    def __init__(self, input_size, learning_rate , epochs ):
+        self.weights = np.zeros(input_size + 1)
+        self.lr = learning_rate
+        self.epochs = epochs
+
+    def activation_function(self, x):
+        return  1 if x>=0 else 0
+
+    def predict(self,x):
+        z = np.dot(x, self.weights[1:]) + self.weights[0]
+        return self.activation_function(z)
+
+    def train(self, x, y):
+        for epoch in range(self.epochs):
+            print(f"Epoch: {epoch +1}:")
+            for xi,target in zip(x,y):
+                output = self.predict(xi)
+                error = target - output
+                print(self.weights[0])
+                self.weights[1:] += self.lr * error * xi
+                self.weights[0] += self.lr * error
+                print(self.weights[0])
+                print(f"  Entrada: {xi}, Esperado: {target}, Predicho: {output}, Error: {error}")
+
+#Ciclo principal
+if __name__ == "__main__":
+    #Leer archivo Excel
+    archivo_excel = pd.read_excel('Recursos/CompuertaXOR.xlsx')
+    #Extraer solo las columnas X1, X2, X3, X4
+    X = archivo_excel[["X1", "X2", "X3", "X4"]].values
+    Y = archivo_excel["d"].values
+
+    #Crear y entrenar el Perceptron
+    perceptronXOR = PerceptronSimple(4,0.001,15)
+    perceptronXOR.train(X,Y)
+
+# Pruebas
+print("\nPruebas finales:")
+for xi in X:
+    print(f"Entrada: {xi}, Predicción: {perceptronXOR.predict(xi)}")
+
+
